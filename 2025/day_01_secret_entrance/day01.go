21ac97d2aff7c53350e480182d0b16f5c6d63baa
Wouter van Hoof <w.p.h.j.v.hoof@student.tue.nl>
2025-12-04 17:07:38 +0100

    feat: day 1 part 2

--- 2025/day_01_secret_entrance/day01.go
+++ 2025/day_01_secret_entrance/day01.go
@@ -8,8 +8,11 @@ import (
 	"strconv"
 )
 
+func mod(a, b int) int {
+	return (a%b + b) % b
+}
+
 func main() {
-	// TODO read file name from argument
 	file, err := os.Open("./input.txt")
 	if err != nil {
 		log.Fatal(err)
@@ -17,7 +20,9 @@ func main() {
 	defer file.Close()
 
 	dial := 50
-	password := 0
+	password1 := 0
+	password2 := 0
+
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		direction := 1
@@ -25,21 +30,36 @@ func main() {
 			direction = -1
 		}
 
-		step, err := strconv.ParseInt(scanner.Text()[1:], 10, 0)
+		step, err := strconv.Atoi(scanner.Text()[1:])
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		dial = (dial + int(step)*direction) % 100
+		inc2 := step / 100
+		step = step % 100
 
-		if dial == 0 {
-			password++
+		prevDial := dial
+		dial = dial + step*direction
+
+		if prevDial != 0 && (dial < 0 || dial > 100) {
+			inc2++
 		}
+
+		dial = mod(dial, 100)
+
+		if dial == 0 && prevDial != 0 {
+			password1++
+			inc2++
+		}
+
+		fmt.Printf("dial : %d, incremented %d\n", dial, inc2)
+		password2 += inc2
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("Password: %d\n", password)
+	fmt.Printf("Password (part 1): %d\n", password1)
+	fmt.Printf("Password (part 2): %d\n", password2)
 }
